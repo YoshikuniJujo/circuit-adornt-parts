999ce4aed3d7ac2b7e953b2a96b053296a36922f
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-08-13 12:23:02 +0900

    refactoring

--- src/Circuit/Adornt/Parts.hs
+++ src/Circuit/Adornt/Parts.hs
@@ -4,15 +4,15 @@ module Circuit.Adornt.Parts (nandGate, norGate) where
 
 import Circuit.Adornt.Builder
 
-norGate, nandGate :: CircuitBuilder Wire21
-norGate = do
-	(oa, ob, oo) <- orGate
-	(ni, no) <- notGate
-	connectWire64 oo ni
-	return (oa, ob, no)
-
+nandGate, norGate :: CircuitBuilder Wire21
 nandGate = do
 	(aa, ab, ao) <- andGate
 	(ni, no) <- notGate
 	connectWire64 ao ni
 	return (aa, ab, no)
+
+norGate = do
+	(oa, ob, oo) <- orGate
+	(ni, no) <- notGate
+	connectWire64 oo ni
+	return (oa, ob, no)
