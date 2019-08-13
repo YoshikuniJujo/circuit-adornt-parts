5b836424e55ac4f9aadea3f8143a9ec57ceba214
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-08-13 13:23:37 +0900

    xorGate

--- src/Circuit/Adornt/Parts.hs
+++ src/Circuit/Adornt/Parts.hs
@@ -1,6 +1,6 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Circuit.Adornt.Parts (nandGate, norGate) where
+module Circuit.Adornt.Parts (nandGate, norGate, xorGate) where
 
 import Circuit.Adornt.Builder
 
@@ -16,3 +16,16 @@ norGate = do
 	(ni, no) <- notGate
 	connectWire64 oo ni
 	return (oa, ob, no)
+
+xorGate :: CircuitBuilder Wire21
+xorGate = do
+	(ain, aout) <- idGate
+	(bin, bout) <- idGate
+	(aa, ab, ao) <- andGate
+	(na, nb, no) <- norGate
+	(ad, nor, xo) <- norGate
+	connectWire64 aout `mapM_` [aa, na]
+	connectWire64 bout `mapM_` [ab, nb]
+	connectWire64 ao ad
+	connectWire64 no nor
+	return (ain, bin, xo)
