43482446b580d26a674d759e4ec69b46c7efe617
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-08-15 09:21:42 +0900

    mux2, mux3, mux4

--- src/Circuit/Adornt/Parts.hs
+++ src/Circuit/Adornt/Parts.hs
@@ -2,96 +2,32 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Circuit.Adornt.Parts (
+	-- * Simple Parts
 	xorGate, nandGate, norGate, andNotBGate, orNotBGate,
+	-- * 2, 3, 4 Input Wires
+	mux2, mux3, mux4,
+	-- * Multiple Input Wires
 	multiple, decoder, multiplexer ) where
-
-import Control.Arrow
-import Control.Monad
-import Data.Foldable
-import Data.Word
 
 import Circuit.Adornt.Builder
 
+import Circuit.Adornt.PartsBasic
 import Tools
 
-nandGate, norGate :: CircuitBuilder Wire21
-nandGate = do
-	(aa, ab, ao) <- andGate
-	(ni, no) <- notGate
-	connectWire64 ao ni
-	return (aa, ab, no)
+mux2 :: CircuitBuilder Wire31
+mux2 = do
+	(sl, is, o) <- multiplexer 2
+	let	(i0, i1) = listToTuple2 is
+	return (sl, i0, i1, o)
 
-norGate = do
-	(oa, ob, oo) <- orGate
-	(ni, no) <- notGate
-	connectWire64 oo ni
-	return (oa, ob, no)
+mux3 :: CircuitBuilder (IWire, IWire, IWire, IWire, OWire)
+mux3 = do
+	(sl, is, o) <- multiplexer 3
+	let	(i0, i1, i2) = listToTuple3 is
+	return (sl, i0, i1, i2, o)
 
-xorGate :: CircuitBuilder Wire21
-xorGate = do
-	(ain, aout) <- idGate
-	(bin, bout) <- idGate
-	(aa, ab, ao) <- andGate
-	(na, nb, no) <- norGate
-	(ad, nor, xo) <- norGate
-	connectWire64 aout `mapM_` [aa, na]
-	connectWire64 bout `mapM_` [ab, nb]
-	connectWire64 ao ad
-	connectWire64 no nor
-	return (ain, bin, xo)
-
-andNotBGate, orNotBGate :: CircuitBuilder Wire21
-andNotBGate = do
-	(ni, no) <- notGate
-	(aa, ab, ao) <- andGate
-	connectWire64 no ab
-	return (aa, ni, ao)
-
-orNotBGate = do
-	(ni, no) <- notGate
-	(aa, ab, ao) <- orGate
-	connectWire64 no ab
-	return (aa, ni, ao)
-
-multiple :: CircuitBuilder Wire21 -> Word16 -> CircuitBuilder ([IWire], OWire)
-multiple _ 0 = ([] ,) <$> constGate 0
-multiple _ 1 = first (: []) <$> idGate
-multiple g 2 = (\(a, b, o) -> ([a, b], o)) <$> g
-multiple g n = do
-	(is1, o1) <- multiple g (n `div` 2)
-	(is2, o2) <- multiple g (n - n `div` 2)
-	(a, b, o) <- g
-	connectWire64 o1 a
-	connectWire64 o2 b
-	return (is1 ++ is2, o)
-
-decoderGen :: Word16 -> CircuitBuilder ([IWire], [OWire])
-decoderGen n = do
-	(is, ois) <- unzip <$> fromIntegral m `replicateM` idGate
-	(ias, oas) <- unzip <$> fromIntegral n `replicateM` multiple andGate m
-	zipWithM_ ((sequence_ .) . flip (zipWith3 id) ois)
-		(binary (inverse, obverse) m) ias
-	return (is, oas)
-	where m = log2 n
-
-decoder :: Word16 -> CircuitBuilder (IWire, [OWire])
-decoder n = do
-	(iin, iout) <- idGate
-	(is, decs) <- decoderGen n
-	for_ (zip [0 ..] is) $ \(ix, iw) -> connectWire (iout, 1, ix) (iw, 1, 0)
-	return (iin, decs)
-
-inverse, obverse :: OWire -> IWire -> CircuitBuilder ()
-inverse o i = do
-	(ni, no) <- notGate
-	zipWithM_ connectWire64 [o, no] [ni, i]
-obverse = connectWire64
-
-multiplexer :: Word16 -> CircuitBuilder (IWire, [IWire], OWire)
-multiplexer n = do
-	(slin, douts) <- decoder n
-	(as, bs, os) <- unzip3 <$> fromIntegral n `replicateM` andGate
-	(ois, oo) <- multiple orGate n
-	zipWithM_ connectWire0_64 douts as
-	zipWithM_ connectWire64 os ois
-	return (slin, bs, oo)
+mux4 :: CircuitBuilder (IWire, IWire, IWire, IWire, IWire, OWire)
+mux4 = do
+	(sl, is, o) <- multiplexer 4
+	let	(i0, i1, i2, i3) = listToTuple4 is
+	return (sl, i0, i1, i2, i3, o)
